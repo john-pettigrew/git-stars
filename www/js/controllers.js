f8e431c37a3aab57664812b28e37abeed9761f48
John Pettigrew <john.pettigrew404@gmail.com>
2014-08-10 02:37:23 -0500

    Add ability to enter username.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,25 @@
 angular.module('starter.controllers', [])
 
-.controller('UserCtrl', function($scope, $http, User) {
-	$scope.test = function(){
-		console.log(User);
-		$http({method: 'GET', url: 'https://api.github.com/users/octocat/starred'}).
-			success(function(data, status, headers, config){
-				console.log(data);
-				User.data = data;
-			}).
-			error(function(data, status, headers, config){
-				console.log('not cool');
-				data = null;
-			});
+.controller('UserCtrl', function($scope, User) {
+	$scope.set = function(username){
+		User.name = username;
+		console.log(User.name);		
 	}
 })
 
-.controller('StarsCtrl', function($scope, User) {
-	$scope.data = User.data;
+.controller('StarsCtrl', function($scope, $http, User) {
+	$scope.refresh = function(){
+		console.log(User.name);
+		if(User.name !== undefined){
+			$http({method: 'GET', url: 'https://api.github.com/users/'+User.name+'/starred'}).
+				success(function(data, status, headers, config){
+					$scope.data = data;
+				}).
+				error(function(data, status, headers, config){
+					console.log('not cool');
+					data = null;
+				});
+		}
+	}
+
 });
