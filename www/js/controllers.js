204c9e255ad26cd734e9025c98d9f4adc3801e17
John Pettigrew <john.pettigrew404@gmail.com>
2014-08-09 23:52:58 -0500

    first commit.

---
+++ www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('starter.controllers', [])
+
+.controller('UserCtrl', function($scope) {
+})
+
+.controller('StarsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
